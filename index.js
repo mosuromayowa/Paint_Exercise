7af6e5e3a35a7718a052ecfcf19fa4db65f21057
mosuromayowa <mosuromayowa@gmail.com>
2023-12-05 20:08:00 -0500

    function to start drawing

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const canvas = document.getElementById('canvas')
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    let drawing = false;
+    let erasing = false;
+    let brushSize = 5;
+    let brushColor = '#000' ;
+
+    function startDrawing (e) {
+        drawing = true;
+    }
+
+
+
+
+
 }
